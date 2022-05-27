9c0f7d2fc6a7c29f22f276d271cbe50947da3615
xiexianhui <xiexianhuicn@outlook.com>
2022-05-27 12:08:22 +0800

    axios

--- src/server/http.js
+++ src/server/http.js
@@ -1,64 +1,85 @@
 import axios from "axios";
 import baseURL from "@/common/config";
+import {ElMessage} from "element-plus";
+
 axios.defaults.baseURL = baseURL
 axios.defaults.withCredentials = true
 axios.defaults.timeout = 15000
 
-axios.interceptors.request.use(
-    config => {
+axios.interceptors.request.use(config => {
     config.headers['time'] = new Date().getTime();
     return config;
-    },
-        error => {
-        return error
-    })
-//响应拦截器即异常处理
-axios.interceptors.response.use(response => {
-    return response
-}, err => {
-    if (err && err.response) {
-        switch (err.response.status) {
-            case 400:
-               console.log('错误请求') ;
-                break;
-            case 401:
-                console.log('未授权，请重新登录');
-                break;
-            case 403:
-                console.log('拒绝访问');
-                break;
-            case 404:
-                console.log('请求错误,未找到该资源');
-                break;
-            case 405:
-                console.log('请求方法未允许');
-                break;
-            case 408:
-                console.log('请求超时');
-                break;
-            case 500:
-                console.log('服务器端出错');
-                break;
-            case 501:
-                console.log('网络未实现');
-                break;
-            case 502:
-                console.log('网络错误');
-                break;
-            case 503:
-                console.log('服务不可用');
-                break;
-            case 504:
-                console.log('网络超时');
-                break;
-            case 505:
-                console.log('http版本不支持该请求');
-                break;
-            default:
-                console.log(`连接错误${err.response.status}`);
-        }
-    } else {
-        console.log('连接到服务器失败')
-    }
-    return Promise.resolve(err.response);
 })
+//响应拦截器即异常处理
+axios.interceptors.response.use(
+    response => {
+        if (response.data.success) {
+            // response.data 是解决数据的获取繁琐问题
+            return response.data
+        } else {
+            ElMessage({
+                showClose: true,
+                message: response.data.message,
+                type: 'error',
+            })
+            // 如果返回的状态失败，需要抛出错误的信息
+            return Promise.reject(new Error(response.data.message))
+        }
+    },
+    error => {
+        ElMessage({
+            showClose: true,
+                message: error.response.data,
+                type: 'error',
+        })
+        return Promise.reject(error)
+    }
+)
+
+export const ajax = {
+    //封装get请求
+    get(url,params) {
+        return new Promise((resolve,reject) => {
+            axios.get(url,{
+                params:params
+            }).then(response => {
+                resolve(response.data);
+            }).catch(err => {
+                reject(err);
+            })
+        });
+    },
+    //封装post请求
+    post(url,data,params){
+        return new Promise((resolve,reject) => {
+            axios.post(url,data,params)
+                .then(response => {
+                    resolve(response.data);
+                },err => {
+                    reject(err);
+                })
+        });
+    },
+    //封装put请求
+    put(url,data,params){
+        return new Promise((resolve,reject) => {
+            axios.put(url,data,params)
+                .then(response => {
+                    resolve(response.data);
+                },err => {
+                    reject(err);
+                })
+        });
+    },
+    //封装post请求
+    delete(url,params){
+        return new Promise((resolve,reject) => {
+            axios.delete(url,params)
+                .then(response => {
+                    resolve(response.data);
+                },err => {
+                    reject(err);
+                })
+        });
+    }
+};

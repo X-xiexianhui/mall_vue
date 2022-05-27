2f412f58bfba464992c01732f8f8b23e2f015b74
xiexianhui <xiexianhuicn@outlook.com>
2022-05-27 23:24:07 +0800

    router

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <mall-header/>
+    <picture-card/>
   </div>
 </template>
 
 <script>
 import MallHeader from "@/components/header/mallHeader";
+import PictureCard from "@/components/product/pictureCard";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "index",
-  components: {MallHeader}
+  components: {PictureCard, MallHeader}
 }
 </script>
 

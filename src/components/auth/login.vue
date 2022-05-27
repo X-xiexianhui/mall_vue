82b592f15b47893c8afc0b0887ec1ec823b563ea
xiexianhui <xiexianhuicn@outlook.com>
2022-05-27 19:13:31 +0800

    index

--- src/components/auth/login.vue
+++ src/components/auth/login.vue
@@ -1,11 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <mall-header/>
+  </div>
 </template>
 
 <script>
+import MallHeader from "@/components/header/mallHeader";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'login'
+  name: 'login',
+  components: {MallHeader}
 }
 </script>
 
---
+++ src/components/header/mallHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="$style.box">
+    <div :class="$style.logo"><img src='@/assets/frontend/img/logo_top.jpg' :class="$style.logo" alt="LeastMall商城"></div>
+    <div :class="$style.btn">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mallHeader"
+}
+</script>
+
+<style module lang="less">
+.box {
+  top: 0;
+  position: fixed;
+  margin: 0 0;
+  padding: 0 0;
+  height: auto;
+  width: 100%;
+  background: orange;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 0 8px 1px rgba(0,0,0,.3);
+  z-index: 3;
+}
+.logo {
+  vertical-align: top;
+  height: 40px;
+}
+.headerMenuBox {
+  display: flex;
+  background: #fff;
+  align-items: flex-start;
+
+  /*justify-content: center*/
+  h1 {
+    text-shadow: 0 0 40px #409EFF;
+    font-size: 26px;
+    text-transform: uppercase;
+    z-index: 10;
+  }
+}
+.menu {
+  margin-left: auto;
+  background: #ffffff;
+}
+.btn{
+  margin-right: 20px;
+}
+</style>
+

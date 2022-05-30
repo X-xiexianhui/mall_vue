51a8a6bdfaae2c3fb0ec24df806fb82cc6e53b5d
xiexianhui <xiexianhuicn@outlook.com>
2022-05-30 11:02:55 +0800

    banner

--- src/components/backend/productList.vue
+++ src/components/backend/productList.vue
@@ -1,31 +1,19 @@
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column label="Date" width="180">
+    <el-table-column label="图片" width="180">
       <template #default="scope">
         <div style="display: flex; align-items: center">
-          <el-icon><timer /></el-icon>
-          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          <img :src="'data:image/png;base64,'+scope.row.image"/>
         </div>
       </template>
     </el-table-column>
-    <el-table-column label="Name" width="180">
-      <template #default="scope">
-        <el-popover effect="light" trigger="hover" placement="top" width="auto">
-          <template #default>
-            <div>name: {{ scope.row.name }}</div>
-            <div>address: {{ scope.row.address }}</div>
-          </template>
-          <template #reference>
-            <el-tag>{{ scope.row.name }}</el-tag>
-          </template>
-        </el-popover>
-      </template>
-    </el-table-column>
+    <el-table-column prop="product" label="关联商品" width="180"></el-table-column>
     <el-table-column label="操作">
       <template #default="scope">
-        <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
-        >修改</el-button
-        >
+        <el-button
+            size="small"
+            @click="handleEdit(scope.$index, scope.row)"
+        >修改</el-button>
         <el-button
             size="small"
             type="danger"

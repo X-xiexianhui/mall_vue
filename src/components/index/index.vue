f4bc841778a6bea062f5d2ff3dfc11ef5edd4c12
xiexianhui <xiexianhuicn@outlook.com>
2022-06-03 16:32:44 +0800

    proxy

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -23,7 +23,7 @@ export default {
       banner: []
     }
   },
-  setup() {
+  created() {
     this.getBanner()
   },
   methods: {
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8080', //目标接口域名
+                target: 'http://localhost:81', //目标接口域名
                 secure: false, //false为http访问，true为https访问
                 changeOrigin: true, //是否跨域
                 pathRewrite: {

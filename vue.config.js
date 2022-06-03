1e70dfcff874efcf444079657b5ea00c3e067b92
xiexianhui <xiexianhuicn@outlook.com>
2022-06-03 16:01:45 +0800

    proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
+    outputDir: "dist",    //打包的目标目录
+    assetsDir: "static",
+    indexPath: 'index.html',
+    // eslint-loader 是否在保存的时候检查
+    lintOnSave: true,
+    // 生产环境是否生成 sourceMap 文件
+    productionSourceMap: false,
+    // 服务项配置
+    devServer: {
+        host:'0.0.0.0',
+        port: 8080,
+        hot: true,//hot配置是否启用模块的热替换功能，devServer的默认行为是在发现源代码被变更后，通过自动刷新整个页面来做到事实预览，开启hot后，将在不刷新整个页面的情况下通过新模块替换老模块来做到实时预览。
+        https: false,
+        hotOnly: false,// hot 和 hotOnly 的区别是在某些模块不支持热更新的情况下，前者会自动刷新页面，后者不会刷新页面，而是在控制台输出热更新失败
+        proxy: {
+            '/apis': {
+                target: 'http://localhost:81', //目标接口域名
+                secure: false, //false为http访问，true为https访问
+                changeOrigin: true, //是否跨域
+                pathRewrite: {
+                    '^/apis': '' //重写接口
+                }
+            }
+        }
+    }
 })
